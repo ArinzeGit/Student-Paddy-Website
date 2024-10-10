9a26ad25ee9ae40e986a7e96eee2d0d50fc0531a
Arinze Owoh <arinzeowoh@gmail.com>
2024-10-10 06:54:54 +0100

    create svg components for nav bar

---
+++ src/components/svg-components/NavLinkLine.tsx
@@ -0,0 +1,16 @@
+const NavLinkLine = () => {
+  return (
+    <svg
+      width="100%"
+      height="3"
+      viewBox="0 0 95 3"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      preserveAspectRatio="none"
+    >
+      <rect width="100%" height="3" fill="currentColor" />
+    </svg>
+  );
+};
+
+export default NavLinkLine;
