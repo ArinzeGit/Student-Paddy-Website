715da40f5ebc4fa6493f61f796ed41a85e05f126
Arinze Owoh <arinzeowoh@gmail.com>
2024-10-19 03:33:02 +0100

    implement footer in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 // import NavBar from "@/components/NavBar";
 import NavBar from "@/components/NavBar";
 import "./globals.css";
+import Footer from "@/components/Footer";
 export const metadata = {
   title: "Student Paddy website",
   description: "Student Paddy website",
@@ -19,7 +20,9 @@ export default function RootLayout({
         </nav>
         <div className="overflow-y-auto no-scrollbar">
           <main>{children}</main>
-          <footer></footer>
+          <footer>
+            <Footer />
+          </footer>
         </div>
       </body>
     </html>
