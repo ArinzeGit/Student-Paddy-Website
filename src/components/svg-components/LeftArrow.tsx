ca4e76aa45ebc08eae9e707759766403e9c738cd
Arinze Owoh <arinzeowoh@gmail.com>
2024-10-24 12:37:00 +0100

    create left and right arrow navigation components for TestimonialCarousel

---
+++ src/components/svg-components/LeftArrow.tsx
@@ -0,0 +1,27 @@
+const LeftArrow = () => {
+  return (
+    <svg
+      width="40"
+      height="40"
+      viewBox="0 0 40 40"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <circle
+        cx="20"
+        cy="20"
+        r="20"
+        transform="matrix(-1 0 0 1 40 0)"
+        fill="currentColor"
+      />
+      <path
+        d="M24 13L17 19.825L24 26"
+        stroke="#0F7863"
+        strokeWidth="2"
+        strokeLinecap="round"
+      />
+    </svg>
+  );
+};
+
+export default LeftArrow;
---
+++ src/components/svg-components/RightArrow.tsx
@@ -0,0 +1,21 @@
+const RightArrow = () => {
+  return (
+    <svg
+      width="40"
+      height="40"
+      viewBox="0 0 40 40"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <circle cx="20" cy="20" r="20" fill="currentColor" />
+      <path
+        d="M16 13L23 19.825L16 26"
+        stroke="#0F7863"
+        strokeWidth="2"
+        strokeLinecap="round"
+      />
+    </svg>
+  );
+};
+
+export default RightArrow;
