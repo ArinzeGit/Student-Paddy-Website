5bfe1b3a4c799a5d1c28f7af7627e71806bffec8
Arinze Owoh <arinzeowoh@gmail.com>
2024-10-27 15:27:36 +0100

    feat: add responsiveness to OpenQuotation and CloseQuotation components

--- src/components/svg-components/OpenQuotation.tsx
+++ src/components/svg-components/OpenQuotation.tsx
@@ -1,8 +1,7 @@
 const OpenQuotation = () => {
   return (
     <svg
-      width="61"
-      height="93"
+      className="w-[clamp(29px,calc(3vw+17.7px),61px)]"
       viewBox="0 0 61 93"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
