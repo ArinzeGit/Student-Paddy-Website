8dc25bc48dc05057b5689056705c58761969dd70
Arinze Owoh <arinzeowoh@gmail.com>
2024-10-27 06:57:52 +0100

    chore(smtp): minor edit in smtp-test.js for request validation

--- smtp-test.js
+++ smtp-test.js
@@ -1,19 +1,28 @@
+
 const nodemailer = require("nodemailer");
 
-const testTransporter = nodemailer.createTransport({
-  host: "smtp-mail.outlook.com",
+const transporter = nodemailer.createTransport({
+  host: "smtp.office365.com",
   port: 587,
-  secure: false, // Use TLS
+  secure: false,
   auth: {
-    user: "no-reply@studentpaddy.com", // Your email
-    pass: process.env.EMAIL_PASSWORD,  // Your password from environment variables
+    user: "no-reply@studentpaddy.com",
+    pass: process.env.EMAIL_PASSWORD,
   },
+  
 });
 
-testTransporter.verify((error, success) => {
+const mailOptions = {
+  from: "no-reply@studentpaddy.com",
+  to: "arinzeowoh@gmail.com",
+  subject: "Hello from Nodemailer!",
+  text: "This is a test email sent from a Node.js script using Outlook SMTP.",
+};
+
+transporter.sendMail(mailOptions, (error, info) => {
   if (error) {
-    console.log("Error with SMTP connection:", error);
+    console.log("Error occurred:", error);
   } else {
-    console.log("SMTP connection is valid!");
+    console.log("Email sent:", info.response);
   }
 });
