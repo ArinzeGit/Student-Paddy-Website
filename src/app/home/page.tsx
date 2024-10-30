a3f8eed04adf082e996e14cd0c2d390ab60a12e0
Arinze Owoh <arinzeowoh@gmail.com>
2024-10-30 04:21:37 +0100

    Enhance responsiveness with updated styles for various screen sizes

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 import LogosMarquee from "@/components/LogosMarquee";
+import StatCard from "@/components/StatCard";
 import CloseQuotation from "@/components/svg-components/CloseQuotation";
 import OpenQuotation from "@/components/svg-components/OpenQuotation";
 import TestimonialCarousel from "@/components/TestimonialCarousel";
@@ -19,93 +20,27 @@ const Home = () => {
         <div className="w-[min(86%,1800px)] mx-auto">
           <RealTimeClock />
 
-          <div className="flex gap-[23px] mt-[38px]">
-            <div className="bg-[white] rounded-[8px] py-[55px] px-[20px] font-avenir flex justify-center items-center relative flex-1">
-              <div className="bg-[rgba(11,138,0,0.15)] rounded-[50px] py-[3px] px-[5px] absolute top-[19px] left-[31px]">
-                <Image
-                  src="/images/icon-trending-up.svg"
-                  alt="Trending up"
-                  width={15}
-                  height={14}
-                  className="inline-block mr-[4px]"
-                />
-                <span className="text-[#0B8A00] font-[400] text-[14px] leading-[1.43]">
-                  10.0%
-                </span>
-              </div>
-              <p className="text-[#000000] font-[500] text-[56px] leading-[1.37]">
-                5 Interns
-              </p>
-              <div className="flex gap-[18px] absolute w-full justify-center items-center bottom-[-33px] left-[50%] translate-x-[-50%] translate-y-[100%]">
-                <Image
-                  src="/images/icon-total-placements.svg"
-                  alt="Total placements"
-                  width={44}
-                  height={44}
-                />
-                <span className="text-[#FFFFFF] font-[800] text-[20px] font-avenir leading-[1.37] tracking-1">
-                  Total Placements
-                </span>
-              </div>
-            </div>
+          <div className="flex flex-col tablet:flex-row gap-[100px] tablet:gap-[2.3%] mt-[38px] w-[60%] tablet:w-full mx-auto">
+            <StatCard
+              percentage={10}
+              value="5 Interns"
+              name="Total Placements"
+              iconSrc="/images/icon-total-placements.svg"
+            />
 
-            <div className="bg-[white] rounded-[8px] py-[55px] px-[20px] font-avenir flex justify-center items-center relative flex-1">
-              <div className="bg-[rgba(11,138,0,0.15)] rounded-[50px] py-[3px] px-[5px] absolute top-[19px] left-[31px]">
-                <Image
-                  src="/images/icon-trending-up.svg"
-                  alt="Trending up"
-                  width={15}
-                  height={14}
-                  className="inline-block mr-[4px]"
-                />
-                <span className="text-[#0B8A00] font-[400] text-[14px] leading-[1.43]">
-                  10.0%
-                </span>
-              </div>
-              <p className="text-[#000000] font-[500] text-[56px] leading-[1.37]">
-                $500
-              </p>
-              <div className="flex gap-[18px] absolute w-full justify-center items-center bottom-[-33px] left-[50%] translate-x-[-50%] translate-y-[100%]">
-                <Image
-                  src="/images/icon-total-stipends.svg"
-                  alt="Total Stipends Disbursed"
-                  width={44}
-                  height={44}
-                />
-                <span className="text-[#FFFFFF] font-[800] text-[20px] font-avenir leading-[1.37] tracking-1">
-                  Total Stipends Disbursed
-                </span>
-              </div>
-            </div>
+            <StatCard
+              percentage={10}
+              value="$500"
+              name="Total Stipends Disbursed"
+              iconSrc="/images/icon-total-stipends.svg"
+            />
 
-            <div className="bg-[white] rounded-[8px] py-[55px] px-[20px] font-avenir flex justify-center items-center relative flex-1">
-              <div className="bg-[rgba(11,138,0,0.15)] rounded-[50px] py-[3px] px-[5px] absolute top-[19px] left-[31px]">
-                <Image
-                  src="/images/icon-trending-up.svg"
-                  alt="Trending up"
-                  width={15}
-                  height={14}
-                  className="inline-block mr-[4px]"
-                />
-                <span className="text-[#0B8A00] font-[400] text-[14px] leading-[1.43]">
-                  10.0%
-                </span>
-              </div>
-              <p className="text-[#000000] font-[500] text-[56px] leading-[1.37]">
-                400 hrs
-              </p>
-              <div className="flex gap-[18px] absolute w-full justify-center items-center bottom-[-33px] left-[50%] translate-x-[-50%] translate-y-[100%]">
-                <Image
-                  src="/images/icon-total-upskilling-hours.svg"
-                  alt="Total Up-skilling Hours"
-                  width={44}
-                  height={44}
-                />
-                <span className="text-[#FFFFFF] font-[800] text-[20px] font-avenir leading-[1.37] tracking-1">
-                  Total Up-skilling Hours
-                </span>
-              </div>
-            </div>
+            <StatCard
+              percentage={10}
+              value="400 hrs"
+              name="Total Up-skilling Hours"
+              iconSrc="/images/icon-total-upskilling-hours.svg"
+            />
           </div>
         </div>
       </div>
@@ -126,13 +61,13 @@ const Home = () => {
         </div>
       </div>
 
-      <div className="bg-[#FFFFFF] relative pt-[360px] pb-[60px]">
-        <div className="absolute top-0 left-0 w-full h-[292px] bg-white transform skew-y-[-6deg] origin-top-left flex flex-col justify-end gap-[25%]">
+      <div className="bg-[#FFFFFF] relative pt-[min(calc(22vw+68px),360px)] pb-[60px]">
+        <div className="absolute top-0 left-0 w-full h-[min(22vw,292px)] bg-white transform skew-y-[-6deg] origin-top-left flex flex-col justify-end gap-[25%]">
           <div className=" h-[25%] bg-[#0F7863]" />
           <div className="h-[25%] bg-[#0F7863]" />
         </div>
 
-        <div className="w-[min(80%,1800px)] mx-auto flex gap-[5%] text-[#0F7863] font-[800] text-[min(3vw,44px)] font-avenir leading-[1.6] justify-center items-center text-center">
+        <div className="w-[min(86%,1800px)] mx-auto flex gap-[5%] text-[#0F7863] font-[800] text-[max(17px,5vw)] tablet:text-[min(44px,calc(1.7vw+19.52px))] font-avenir leading-[1.6] justify-center items-center text-center">
           <div>
             <OpenQuotation />
           </div>
@@ -153,27 +88,28 @@ const Home = () => {
 
       <div className="bg-[#ECF3F1] pt-[75px] pb-[calc(10.52vw+32px)]">
         {/* 100vw*tan(6)=10.52vw is to take care of encroaching slant section and 32px is an extra padding away from slant section */}
-        <div className="w-[min(86%,1800px)] mx-auto flex flex-col gap-[25px]">
-          <div className="flex gap-[5%] items-center">
-            <div className="flex items-start gap-[35px] w-[50%]">
+        <div className="w-[min(86%,1800px)] mx-auto flex flex-col gap-[100px] desktop:gap-[50px]">
+          <div className="flex flex-col desktop:flex-row gap-[5%] items-center">
+            <div className="flex items-start gap-[7%] w-full desktop:w-[50%]">
               <Image
                 src="/images/icon-star-in-circle.svg"
                 alt="Star icon"
                 width={92}
                 height={92}
+                className="w-[50px] tablet:w-[92px]"
               />
               <div>
-                <h2 className="text-[#474747] font-[500] text-[min(3vw,44px)] font-avenir leading-[1.6]">
+                <h2 className="text-[#474747] font-[500] text-[max(17px,5vw)] tablet:text-[min(44px,calc(1.7vw+19.52px))] font-avenir leading-[1.6]">
                   Real-World{" "}
                   <span className="text-[#0F7863] font-[800]">Exposure</span>
                 </h2>
-                <p className="mt-[13px] text-[#474747] font-[800] text-[20px] font-avenir leading-[1.75]">
+                <p className="mt-[13px] text-[#474747] font-[800] text-[clamp(15px,calc(1.5vw+9.2px),20px)] font-avenir leading-[1.75]">
                   Get the opportunity to work alongside industry leaders and
                   learn how professionals tackle everyday challenges.
                 </p>
                 <Link
                   href="/contact-us"
-                  className="mt-[26px] inline-block bg-[#0F7863] text-[#FFFFFF] font-[500] text-[18px] font-avenir leading-[1.38] p-[18px] rounded-[50px]"
+                  className="mt-[26px] inline-block bg-[#0F7863] text-[#FFFFFF] font-[500] text-[clamp(14px,calc(1.17vw+7.2px),18px)] font-avenir leading-[1.38] p-[max(9px,2vw)] tablet:p-[18px] rounded-[50px]"
                 >
                   Get Started
                 </Link>
@@ -185,30 +121,31 @@ const Home = () => {
               alt="Real world exposure"
               width={1428}
               height={1649}
-              className="w-[45%]"
+              className="w-[80%] desktop:w-[45%]"
             />
           </div>
 
-          <div className="flex gap-[5%] items-center flex-row-reverse">
-            <div className="flex items-start gap-[35px] w-[50%] flex-row-reverse">
+          <div className="flex flex-col desktop:flex-row-reverse gap-[5%] items-center">
+            <div className="flex items-start gap-[7%] w-full desktop:w-[50%] flex-row desktop:flex-row-reverse">
               <Image
                 src="/images/icon-star-in-circle.svg"
                 alt="Star icon"
                 width={92}
                 height={92}
+                className="w-[50px] tablet:w-[92px]"
               />
-              <div className="text-right">
-                <h2 className="text-[#474747] font-[500] text-[min(3vw,44px)] font-avenir leading-[1.6]">
+              <div className="text-left desktop:text-right">
+                <h2 className="text-[#474747] font-[500] text-[max(17px,5vw)] tablet:text-[min(44px,calc(1.7vw+19.52px))] font-avenir leading-[1.6]">
                   Guided{" "}
                   <span className="text-[#0F7863] font-[800]">Mentorship</span>
                 </h2>
-                <p className="mt-[13px] text-[#474747] font-[800] text-[20px] font-avenir leading-[1.75]">
+                <p className="mt-[13px] text-[#474747] font-[800] text-[clamp(15px,calc(1.5vw+9.2px),20px)] font-avenir leading-[1.75]">
                   Receive personalized guidance from mentors who help you
                   navigate your career path and provide expert insights.
                 </p>
                 <Link
                   href="/contact-us"
-                  className="mt-[26px] inline-block bg-[#0F7863] text-[#FFFFFF] font-[500] text-[18px] font-avenir leading-[1.38] p-[18px] rounded-[50px]"
+                  className="mt-[26px] inline-block bg-[#0F7863] text-[#FFFFFF] font-[500] text-[clamp(14px,calc(1.17vw+7.2px),18px)] font-avenir leading-[1.38] p-[max(9px,2vw)] tablet:p-[18px] rounded-[50px]"
                 >
                   Sign me up!
                 </Link>
@@ -220,30 +157,31 @@ const Home = () => {
               alt="Guided mentorship"
               width={1304}
               height={1538}
-              className="w-[45%]"
+              className="w-[80%] desktop:w-[45%]"
             />
           </div>
 
-          <div className="flex gap-[5%] items-center">
-            <div className="flex items-start gap-[35px] w-[50%]">
+          <div className="flex flex-col desktop:flex-row gap-[5%] items-center">
+            <div className="flex items-start gap-[7%] w-full desktop:w-[50%]">
               <Image
                 src="/images/icon-star-in-circle.svg"
                 alt="Star icon"
                 width={92}
                 height={92}
+                className="w-[50px] tablet:w-[92px]"
               />
               <div>
-                <h2 className="text-[#474747] font-[500] text-[min(3vw,44px)] font-avenir leading-[1.6]">
+                <h2 className="text-[#474747] font-[500] text-[max(17px,5vw)] tablet:text-[min(44px,calc(1.7vw+19.52px))] font-avenir leading-[1.6]">
                   Industry{" "}
                   <span className="text-[#0F7863] font-[800]">Connections</span>
                 </h2>
-                <p className="mt-[13px] text-[#474747] font-[800] text-[20px] font-avenir leading-[1.75]">
+                <p className="mt-[13px] text-[#474747] font-[800] text-[clamp(15px,calc(1.5vw+9.2px),20px)] font-avenir leading-[1.75]">
                   Expand your professional network by connecting with mentors
                   and employers across various industries.
                 </p>
                 <Link
                   href="/contact-us"
-                  className="mt-[26px] inline-block bg-[#0F7863] text-[#FFFFFF] font-[500] text-[18px] font-avenir leading-[1.38] p-[18px] rounded-[50px]"
+                  className="mt-[26px] inline-block bg-[#0F7863] text-[#FFFFFF] font-[500] text-[clamp(14px,calc(1.17vw+7.2px),18px)] font-avenir leading-[1.38] p-[max(9px,2vw)] tablet:p-[18px] rounded-[50px]"
                 >
                   I'm interested
                 </Link>
@@ -255,30 +193,31 @@ const Home = () => {
               alt="Industry connections"
               width={1650}
               height={1549}
-              className="w-[45%]"
+              className="w-[80%] desktop:w-[45%]"
             />
           </div>
 
-          <div className="flex gap-[5%] items-center flex-row-reverse">
-            <div className="flex items-start gap-[35px] w-[50%] flex-row-reverse">
+          <div className="flex flex-col desktop:flex-row-reverse gap-[5%] items-center">
+            <div className="flex items-start gap-[7%] w-full desktop:w-[50%] flex-row desktop:flex-row-reverse">
               <Image
                 src="/images/icon-star-in-circle.svg"
                 alt="Star icon"
                 width={92}
                 height={92}
+                className="w-[50px] tablet:w-[92px]"
               />
-              <div className="text-right">
-                <h2 className="text-[#474747] font-[500] text-[min(3vw,44px)] font-avenir leading-[1.6]">
+              <div className="text-left desktop:text-right">
+                <h2 className="text-[#474747] font-[500] text-[max(17px,5vw)] tablet:text-[min(44px,calc(1.7vw+19.52px))] font-avenir leading-[1.6]">
                   Career Pathway{" "}
                   <span className="text-[#0F7863] font-[800]">Analytics</span>
                 </h2>
-                <p className="mt-[13px] text-[#474747] font-[800] text-[20px] font-avenir leading-[1.75]">
+                <p className="mt-[13px] text-[#474747] font-[800] text-[clamp(15px,calc(1.5vw+9.2px),20px)] font-avenir leading-[1.75]">
                   Track your development and progress using our personalized
                   analytics, showing how you’re preparing for your career.
                 </p>
                 <Link
                   href="/contact-us"
-                  className="mt-[26px] inline-block bg-[#0F7863] text-[#FFFFFF] font-[500] text-[18px] font-avenir leading-[1.38] p-[18px] rounded-[50px]"
+                  className="mt-[26px] inline-block bg-[#0F7863] text-[#FFFFFF] font-[500] text-[clamp(14px,calc(1.17vw+7.2px),18px)] font-avenir leading-[1.38] p-[max(9px,2vw)] tablet:p-[18px] rounded-[50px]"
                 >
                   Let's go!
                 </Link>
@@ -290,7 +229,7 @@ const Home = () => {
               alt="Career pathway analytics"
               width={1524}
               height={1345}
-              className="w-[45%]"
+              className="w-[80%] desktop:w-[45%]"
             />
           </div>
         </div>
@@ -306,7 +245,7 @@ const Home = () => {
           </div>
         </div>
 
-        <div className="w-[min(90%,1800px)] mx-auto">
+        <div className="w-[min(90%,1884px)] mx-auto">
           <TestimonialCarousel />
         </div>
 
