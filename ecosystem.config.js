d5854bddce7f33a934c4d267ed818428e3654df7
Arinze Owoh <arinzeowoh@gmail.com>
2024-11-26 16:31:51 +0100

    minor edit ecosystem file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     apps: [
-      {      name: "app-name",
+      {      name: "student-paddy-website",
         script: "./node_modules/next/dist/bin/next",
         args: "start -p " + (process.env.PORT || 3000),
         watch: false,
